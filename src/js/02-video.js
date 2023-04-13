2b0983414ff2102a3f20a3494955c1b5a8da4e32
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-04-13 11:53:10 +0200

    дд

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,34 @@
-import Player from '@vimeo/player';
+import Player from '@vimeo/player'
+// var throttle = require('lodash.throttle');
+
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Player('vimeo-player');
+// const iframePlayer = new Vimeo.Player(iframe);
 // тестова частина:   
-console.log(iframePlayer);
-iframePlayer.on('play', function () {
+console.log(player);
+player.on('play', function () {
         console.log('played the video!');
     });
 
-
     //функціональна частина: 
-iframePlayer.on('timeupdate', _.throttle (function(data) {
+player.on('timeupdate', _.throttle (function(data) {
 
      localStorage.setItem("videoplayer-current-time", data.seconds); }, 1000)
     // data is an object containing properties specific to that event
 );
 
 const seconds = localStorage.getItem("videoplayer-current-time");
-iframePlayer.setCurrentTime(seconds).then(function(seconds) {
+player.setCurrentTime(seconds).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
             break;
-
         default:
             // some other error occurred
             break;
     }
 });
+
+localStorage.removeItem("ui-theme");
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,51 @@
 const feedbackForm = document.querySelector(".feedback-form");
-feedbackForm.addEventListener('input', absorbDateForm);
-feedbackForm.addEventListener('submit', absorbDateForm);
-function absorbDateForm(ev) {
-    ev.preventDefault();
-    console.log("kkjj")
-    const {
-        elements: { email, message }
-    } = ev.currentTarget;
-    console.log(elements);
-    localStorage.setItem("feedback-form-state", JSON.stringify(ev.currentTarget));
-}
+feedbackForm.addEventListener('submit', validDateTest);
+feedbackForm.addEventListener('input',  _.throttle(formLocalStorage,300));
 
+importToFormStorage();
+
+function importToFormStorage() {
+    const dataStorage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(dataStorage);
+    if (dataStorage) {
+        for (key in dataStorage) {
+            feedbackForm[key].value = dataStorage[key];
+        }
+    }
+};
+
+function formLocalStorage(event) {
+    event.preventDefault();
+    const {
+            elements: {email, message}
+          } = event.currentTarget;
+  // inputDataForm[event.target.name] = event.target.value;
+  try {
+    const inputDataForm = JSON.stringify(
+      { email: email.value, message: message.value });
+
+    console.log(inputDataForm);
+    localStorage.setItem("feedback-form-state", inputDataForm); 
+    } catch (error) { console.log("error") }
+};
+
+function validDateTest (event) {
+    event.preventDefault();
+    const isValidForm = [...event.target.elements]
+    .filter((elements) => elements.tagName === "INPUT")
+    .some((elements)=> elements.value === "");
+      
+    if (isValidForm) {
+        alert("Усі поля мають бути заповнені.");
+        return
+      }
+        const {
+            elements: {email, message}
+          } = event.currentTarget;
+
+        const resultForm = {email: email.value, message: message.value};
+    console.log(resultForm);
+    feedbackForm.reset();
+    localStorage.removeItem("feedback-form-state");
+return;
+  }; 
