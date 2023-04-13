50f74a0afa26d71b3ca6166bc5abd1c539b47c81
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-04-13 09:53:47 +0000

    Deploying to gh-pages from @ Zmeimor/goit-js-hw-08@2b0983414ff2102a3f20a3494955c1b5a8da4e32 🚀

--- 03-feedback.2700b1d8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");function t(e){e.preventDefault(),console.log("kkjj");var t=e.currentTarget.elements;t.email,t.message;console.log(elements),localStorage.setItem("feedback-form-state",JSON.stringify(e.currentTarget))}e.addEventListener("input",t),e.addEventListener("submit",t)}();
-//# sourceMappingURL=03-feedback.2700b1d8.js.map
