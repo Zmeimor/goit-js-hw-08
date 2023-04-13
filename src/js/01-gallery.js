c610bfdd17f75eb2b78aacade9915b887db95048
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-04-13 12:05:33 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,8 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.js";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
 
